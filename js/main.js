0cbc442193548fd2e0ed83dabeaf17aa10bdd976
rockndraw <rnd.webstudio@gmail.com>
2017-08-22 10:36:41 +0300

    some magic + fonts

--- js/main.js
+++ js/main.js
@@ -28,11 +28,11 @@ $('.galery').slick({
   ],
 });
 
-
-$("body").on("click",".trans-link",function(event){
+$('body').on('click', '.trans-link', function(event) {
   event.preventDefault();
-  var id=$(this).attr('href'),top=$(id).offset().top;
-  $('body,html').animate({scrollTop:top-30},1500);
+  var id = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({ scrollTop: top - 30 }, 1500);
 });
 
 ymaps.ready(function() {
@@ -74,7 +74,7 @@ ymaps.ready(function() {
       {
         hintContent: 'Центральный офис',
         balloonContent: 'Центральный офис',
-      //  iconContent: '12',
+        //  iconContent: '12',
       },
       {
         // Опции.
@@ -93,14 +93,42 @@ ymaps.ready(function() {
         iconContentLayout: MyIconContentLayout,
       },
     );
-  
 
   myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+});
 
+$('.input__field').focusout(function() {
+  if ($(this).val() != '') {
+    $(this).parent().addClass('input--filled');
+  }
 });
 
 $(document).ready(function() {
-    $('.title--red').addClass('animated fadeInLeftBig');
-    $('.title--gray').addClass('animated fadeInRightBig');
-    $('.btn').addClass('animated fadeIn');
+  $('.title--red').addClass('animated fadeInLeftBig');
+  $('.title--gray').addClass('animated fadeInRightBig');
+  setTimeout(function() {
+        $('#home .btn').addClass('animated fadeIn');
+    }, 1000);
+});
+var products = 0;
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 10) {
+    if (products != 1) {
+    $('.counter').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+  });
+  setTimeout(function() {
+        $('#products .btn').addClass('animated fadeIn');
+    }, 1800);
+}
+  products = 1;
+  }
 });
